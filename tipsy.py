e981488db87d9a5bdc3f2ee14a3388dfd87c0b84
Christian <moonlimb@gmail.com>
2012-10-24 16:59:31 -0700

    added a form for new tasks

--- tipsy.py
+++ tipsy.py
@@ -1,7 +1,7 @@
 """
 tipsy.py -- Flask based todo list
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import model
 
 app = Flask(__name__)
@@ -15,6 +15,18 @@ def tasks():
     db = model.connect_db()
     tasks_from_db = model.get_tasks(db, None)
     return render_template('tasks.html', user_name="Erica", task_list=tasks_from_db)
+
+@app.route("/new_task")
+def new_task():
+    db=model.connect_db()
+    return render_template('new_task.html')
+
+@app.route("/save_task", methods=["POST"])
+def save_task():
+    task_title = request.form['task_title']
+    db = model.connect_db()
+    task_id = model.new_task(db, task_title, 1)
+    return "Success!"
 
 if __name__ == "__main__":
     app.run(debug=True)
