274cf4047d92df36eefe0fc6af53b5c9f857913a
Christian <moonlimb@gmail.com>
2012-10-24 12:34:54 -0700

    made some more functions work

--- model.py
+++ model.py
@@ -13,9 +13,9 @@ def connect_db():
 def new_user(db, email, password, name_the_first, name_the_last):
     c=db.cursor()
     query=""" INSERT INTO users VALUES (NULL, ?, ?, ?, ?)"""
-    results = c.execute(query, (email, password, name_the_first, name_the_last))
+    result = c.execute(query, (email, password, name_the_first, name_the_last))
     db.commit()
-    return results.lastrowid 
+    return result.lastrowid 
 '''
 given a username and password, check database to find user and compare password.
 if user exists and username/pw is correct, return a dictionary of user fields from 
@@ -33,31 +33,53 @@ def authenticate(db, user_email, user_password):
 
     return None
 
-def new_task (db, task_title, user_email):
+def new_task (db, task_title, user_id):
     c=db.cursor()
     now = datetime.datetime.now()
     task_created_at = str(now)
     query="""INSERT INTO tasks VALUES (NULL, ?, ?, NULL, ?)"""
-    results = c.execute(query, (task_title, task_created_at, user_email))
+    result = c.execute(query, (task_title, task_created_at, user_id))
     db.commit()
-    return results.lastrowid
-#everything above this comment works! 
+    return result.lastrowid 
 
 def get_user(db, user_id):
     c=db.cursor()
     query="""SELECT * FROM users WHERE id=?"""
-    c.execute(query(user_id))
-    pass
+    c.execute(query, (user_id, ))
+    result=c.fetchone()
+    return result
+    if result: 
+        fields = ['id', 'email', 'password', 'first_name', 'last_name']
+        return dict(zip(fields, result))
+        #everything above this comment works!
 
 def complete_task(db, task_id):
     c=db.cursor()
-    pass
+    now = datetime.datetime.now()
+    complete_time = str(now)
+    query="""UPDATE tasks SET completed_at=? WHERE id =?"""
+    result = c.execute(query, (complete_time, task_id))
+    db.commit()
 
-def get_tasks(db, user_id):
+def get_tasks(db, u_id):
     c=db.cursor()
-    pass
+    if u_id == None:
+        print 'one'
+        query="""SELECT * FROM tasks"""
+        c.execute(query)
+        result=c.fetchall()
+        print result
+        return result
+    else:
+        print 'two'
+        query="""SELECT * FROM tasks WHERE user_id=?"""
+        c.execute(query, (u_id, ))
+        result=c.fetchall()
+        print result
+        return result
 
-def get_tasks(db, task_id):
+
+def get_task(db, task_id):
     c=db.cursor()
     pass
 
